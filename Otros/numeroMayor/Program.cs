fbc83a2ff580a36c66f3420286deb3a29b6b3542
zahid1905 <superpichi.chido@gmail.com>
2021-06-09 19:37:59 -0500

    Add files via upload

---
+++ Otros/numeroMayor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace numeroMayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 0, numero2 = 0, numero3 = 0, mayor = 0;
+
+            Console.WriteLine("Dame el primer numero: ");
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dame el segundo numero: ");
+            numero2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dame el tercer numero: ");
+            numero3 = int.Parse(Console.ReadLine());
+
+            if (numero1 > numero2 && numero1 > numero3)
+            {
+                mayor = numero1;
+            }
+            else
+            {
+                if (numero2 > numero3)
+                {
+                    mayor = numero2;
+                }
+                else
+                {
+                    mayor = numero3;
+                }
+            }
+            
+            Console.WriteLine("El numero mayor es: " + mayor);
+        }
+    }
+}
---
+++ Otros/operarArchivosTexto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace operarArchivosTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un archivo, si existe lo sobreescribe
+            //StreamWriter escritura = File.CreateText(@"/home/zahid/Documentos/ProgramasCSharp/operarArchivosTexto/texto.txt");
+            //Si archivo existe, escribe en ese mismo a partir de la ultima linea, si no existe lo crea
+            //StreamWriter escrituraDos = File.AppendText(@"/home/zahid/Documentos/ProgramasCSharp/operarArchivosTexto/texto.txt");
+
+            StreamReader lectura = File.OpenText(@"/home/zahid/Documentos/ProgramasCSharp/operarArchivosTexto/texto.txt");
+            //Guardar todo a un string
+            string contenido = lectura.ReadToEnd();
+            //Imprimir el string
+            Console.WriteLine(contenido);
+            //Cerrar archivo
+            lectura.Close();
+            //Guardar los datos en un arraglo, separado por saltos de linea
+            string[] elementos = contenido.Split('\n');
+            //Obneter la ultima posiscion del arreglo
+            int ultimo = elementos.Length-2;
+            //Abrir el archivo
+            StreamWriter escritura = File.AppendText(@"/home/zahid/Documentos/ProgramasCSharp/operarArchivosTexto/texto.txt");
+            //Escribe a la ultima linea
+            escritura.WriteLine(int.Parse(elementos[ultimo]) + 1);
+            //Cierra el archivo
+            escritura.Close();
+
+        }
+    }
+}
