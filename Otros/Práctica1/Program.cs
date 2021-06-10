0fef06ddb1e58cfd7eb5dd07e7983ac5a3f06fdf
zahid1905 <superpichi.chido@gmail.com>
2021-06-09 19:38:51 -0500

    Add files via upload

---
+++ Otros/Práctica1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Práctica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            multiplo(130, 5);
+        }
+
+        //Ejercicio 1
+        public static void multiplo(int x, int m) {
+            if (x % m == 0) {
+                Console.WriteLine(x + " es múltiplo de " + m);
+            }
+            else {
+                Console.WriteLine(x + " no es múltiplo de " + m);
+            }
+        }
+    }
+}
---
+++ Otros/p1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa tu nombre completo iniciando por apellidos: ");
+            string nombre = Console.ReadLine();
+
+            string [] nombres = nombre.Split(' ');
+            Console.WriteLine("Tu apellido paterno es: " + nombres[0]);
+            Console.WriteLine("Tu apellido materno es: " + nombres[1]);
+            Console.WriteLine("Tu nombre es: " + nombres[2]);
+
+            Console.WriteLine("Tu nombre tiene " + nombres[0].Length + " letras");
+            Console.WriteLine(nombres[0].Substring(0,1));
+        }
+    }
+}
