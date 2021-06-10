4ea2ddd6e128833582c443772270983a7289a68e
zahid1905 <superpichi.chido@gmail.com>
2021-06-09 19:45:49 -0500

    Add files via upload

---
+++ Otros/swithcRandom/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace swithcRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random rnd = new Random();
+            
+            //int caseSwitch = 2;
+            //int caseSwitch = rnd.Next(1, 4);
+            Console.WriteLine("Que deseas hacer: \n1-Suma\n2-Resta\n3-Multiplicacion\n4-Division\n5-Potencia");
+            byte caseSwitch = byte.Parse(Console.ReadLine());
+
+            double resultado = 0, numero1 = 0, numero2 = 0;
+
+            if (caseSwitch != 5 && caseSwitch < 6)
+            {
+                Console.WriteLine("Ingresa el primer numero a operar: ");
+                numero1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingresa el segundo numero a operar: ");
+                numero2 = double.Parse(Console.ReadLine());
+            }
+
+            else
+            {
+                if (caseSwitch == 5)
+                {
+                    Console.WriteLine("Ingresa el numero a operar: ");
+                    numero1 = double.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine($"Opcion invalida ({caseSwitch})");
+                }
+            }
+
+            switch (caseSwitch)
+            {
+                case 1:
+                    resultado = numero1 + numero2;
+                    break;
+                case 2:
+                    resultado = numero1 - numero2;
+                    break;
+                case 3:                
+                    resultado = numero1 * numero2;
+                    break;
+                case 4:
+                    resultado = numero1 / numero2;
+                    break;
+                case 5:
+                    resultado = numero1 * numero1;
+                    break;
+                default: 
+                    Console.WriteLine($"Opcion invalida ({caseSwitch})");
+                    break;
+            }
+
+            Console.WriteLine("El resultado es: " + resultado);
+        }
+    }
+}
---
+++ Otros/temperaturaEstaciones/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace temperaturaEstaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa todos los datos separados por una coma");
+            string datos = Console.ReadLine();
+            string [] datosEstaciones = datos.Split(',');
+
+            int [] datosEnteros = new int [20];
+
+            int i = 2; //Las primeras dos posiciones no interesan, tienen
+            int i1 = 0;
+            int i2 = 0;
+
+            while (i < 30)
+            {
+                while (i1 < 4)
+                {
+                    datosEnteros[i2] = int.Parse(datosEstaciones[i]);
+                    i2++;
+                    i1++;
+                    i++;
+                }
+                i = i + 2;
+                i1 = 0;
+            }
+
+            int [] temperatura = new int [5];
+
+            i = 0;
+            i1 = 0;
+
+            while (i < 17)
+            {
+                temperatura[i1] = datosEnteros[i];
+                i = i + 4;
+                i1 ++;
+            }
+
+            i2 = 0;
+            int mayor = 0;
+
+            for (i = 0; i < 5; i++)
+            {
+                for (i1 = i + 1; i1 < 5; i1++)
+                {
+                    if (temperatura[i] < temperatura[i1])
+                    {
+                        i2 = temperatura[i1];
+                        temperatura[i1] = temperatura[i];
+                        temperatura[i] = i2;
+                        mayor = i1;
+                    }
+                }
+            }
+
+            Console.Write("La temperatura mayor es " + temperatura[0] + " de la estacion " + mayor);
+            
+        }
+    }
+}
