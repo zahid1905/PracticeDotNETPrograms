3b4fb3e97c34cb4cc974464554d330efd665de3a
zahid1905 <superpichi.chido@gmail.com>
2021-06-09 19:30:32 -0500

    Add files via upload

---
+++ Otros/Ejemplo_3-7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Ejemplo_3_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Precio base por kilo: ");
+            double precioBase = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tipo de uva: \na-Tipo A\nb-Tipo B");
+            string tipo = Console.ReadLine();
+
+            Console.WriteLine("Tamaño de uva: \n1-Tamano 1\n2-Tamano 2");
+            byte tamano = byte.Parse(Console.ReadLine());
+
+            double cargo = 0;
+
+            if (tipo == "a")
+            {
+                if (tamano == 1)
+                {
+                    cargo = 0.20;
+                }
+                else
+                {
+                    cargo = 0.30;
+                }
+            }
+            else
+            {
+                if (tamano == 1)
+                {
+                    cargo = -0.30;
+                }
+                else
+                {
+                    cargo = -0.50;
+                }
+            }
+
+            double total = precioBase + cargo; 
+
+            Console.WriteLine("Subtotal = " + precioBase + "\nCargo = " + cargo + "\nTotal = " + total);
+
+        }
+    }
+}
---
+++ Otros/editar/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+
+namespace editar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Cambio de password");
+
+            Console.WriteLine("Ingresa username");
+            string busqueda = Console.ReadLine();
+                                                    
+            StreamReader lecturaCambios = File.OpenText(@"/home/zahid/Documentos/ProgramasCSharp/proyectoP2/registros/users.txt");
+            string contenidoCambios = lecturaCambios.ReadToEnd();                                        
+            lecturaCambios.Close();
+            string[] elementosCambios = contenidoCambios.Split('\n');
+
+            byte control = 0;
+            
+            for (int z = 1; z < elementosCambios.Length; z=z+5)
+            {
+                if (elementosCambios[z].Trim().Equals(busqueda))
+                {
+                    Console.WriteLine("Ingresa nuevo password");
+                    elementosCambios[z+1] = Console.ReadLine();
+
+                    Console.WriteLine("Cambio de password exitoso");
+
+                    control = 1;
+
+                    break;
+                }
+                else
+                {
+                    control = 0;
+                }
+            }
+            if (control == 0)
+            {
+                Console.WriteLine("Username no existe");
+            }
+
+            StreamWriter escrituraCambios = File.CreateText(@"/home/zahid/Documentos/ProgramasCSharp/proyectoP2/registros/users.txt");
+            for (int z = 0; z < elementosCambios.Length - 1; z++)
+            {
+                escrituraCambios.WriteLine(elementosCambios[z]);
+            }
+            escrituraCambios.Close();
+                                 
+        }
+    }
+}
