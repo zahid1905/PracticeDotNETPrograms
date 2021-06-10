09c51c6c42362d336bc5efeb91cff8a89f30dfeb
zahid1905 <superpichi.chido@gmail.com>
2021-06-09 19:29:41 -0500

    Add files via upload

---
+++ Otros/Csharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, c2 = 0, z, c;
+            float a = 0;
+
+            Console.WriteLine("Ingresa la cantidad de numeros: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            for (c = 1; c <= x; c++)
+            {
+                Console.WriteLine("Ingrese el numero " + c + "\n");
+                z = Convert.ToInt32(Console.ReadLine());
+
+                if (z % 2 != 0)
+                {
+                    a = a + z;
+
+                    c2++;
+                }
+            }
+            Console.WriteLine("Promedio: " + a / c2);
+        }
+    }
+}
